4766f1f0813246912efe12ba37ee40557c310fbf
Rafael de Lima Thomaz <79thomaz@gmail.com>
2023-10-02 20:52:34 -0300

    create models

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+Route::middleware(['auth'])->group(function () {
+    
+    Route::get('/users', function () {
+        return response()->json(['message' => 'List of users']);
+    });
+
+    Route::get('/login', function () {
+        return response()->json(['message' => 'List of users']);
+    });
+});
