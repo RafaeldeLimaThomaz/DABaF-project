73edbcbdd732475562ab919eabf27c90749cc161
Rafael de Lima Thomaz <79thomaz@gmail.com>
2023-10-10 21:45:49 -0300

    fix unit tests

--- tests/Feature/FeaturesTest.php
+++ tests/Feature/FeaturesTest.php
@@ -12,6 +12,29 @@ use Illuminate\Support\Str;
 class FeaturesTest extends TestCase
 {
     /**
+     * Welcome page test.
+     */
+    public function test_welcome_page_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Login page test.
+     */
+    public function test_login_page(): void
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+        $response->assertSee('Login');
+        $response->assertSee('Email');
+        $response->assertSee('Password');
+    }
+
+    /**
      * User login test.
      */
     public function test_user_login()
@@ -96,34 +119,5 @@ class FeaturesTest extends TestCase
             'name' => $user->name,
             'email' => $user->email,
         ]);
-    }
-
-    public function test_create_post_with_tags(): void
-    {
-        $postTitle = Str::random(10);
-        $postDescription = Str::random(10);
-
-        $post = Post::factory()->create([
-            'title' => $postTitle,
-            'description' => $postDescription,
-        ]);
-        
-        $tag1 = Tag::factory()->create(['name' => 'DABaf']);
-        $tag2 = Tag::factory()->create(['name' => 'Laravel']);
-
-        $post->tags()->save($tag1);
-        $post->tags()->save($tag2);
-
-        $this->assertDatabaseHas('posts', [
-            'title' =>  $postTitle,
-            'description' => $postDescription,
-        ]);
-
-        $post = Post::where('title', $postTitle)->first();
-        $this->assertNotNull($post);
-
-        $this->assertTrue($post->tags->contains($tag1));
-        $this->assertTrue($post->tags->contains($tag2));
-
     }
 }
--- tests/Unit/UnitTest.php
+++ tests/Unit/UnitTest.php
@@ -5,33 +5,11 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use App\Models\Post;
 use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Support\Str;
 
 class UnitTest extends TestCase
 {
-    /**
-     * Welcome page test.
-     */
-    public function test_welcome_page_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-
-    /**
-     * Login page test.
-     */
-    public function test_login_page(): void
-    {
-        $response = $this->get('/login');
-
-        $response->assertStatus(200);
-        $response->assertSee('Login');
-        $response->assertSee('Email');
-        $response->assertSee('Password');
-    }
-
     public function test_create_post_with_tags(): void
     {
         $postTitle = Str::random(10);
@@ -58,7 +36,16 @@ class UnitTest extends TestCase
 
         $this->assertTrue($post->tags->contains($tag1));
         $this->assertTrue($post->tags->contains($tag2));
+    }
 
+    public function test_user_creation(): void
+    {
+        $user = User::factory()->create([]);
+        
+        $this->assertDatabaseHas('users', [
+            'name' => $user->name,
+            'email' => $user->email,
+        ]);
     }
     
 }
