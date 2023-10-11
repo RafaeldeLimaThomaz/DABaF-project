809277b85e097a0e95bdb135fccc7409d22e33ea
Rafael de Lima Thomaz <79thomaz@gmail.com>
2023-10-10 21:17:58 -0300

    fix .env example variables

--- tests/Feature/FeaturesTest.php
+++ tests/Feature/FeaturesTest.php
@@ -3,9 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use App\Models\User; 
-use App\Models\Post;
-use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Support\Str;
 
 
@@ -96,34 +94,5 @@ class FeaturesTest extends TestCase
             'name' => $user->name,
             'email' => $user->email,
         ]);
-    }
-
-    public function test_create_post_with_tags(): void
-    {
-        $postTitle = Str::random(10);
-        $postDescription = Str::random(10);
-
-        $post = Post::factory()->create([
-            'title' => $postTitle,
-            'description' => $postDescription,
-        ]);
-        
-        $tag1 = Tag::factory()->create(['name' => 'DABaf']);
-        $tag2 = Tag::factory()->create(['name' => 'Laravel']);
-
-        $post->tags()->save($tag1);
-        $post->tags()->save($tag2);
-
-        $this->assertDatabaseHas('posts', [
-            'title' =>  $postTitle,
-            'description' => $postDescription,
-        ]);
-
-        $post = Post::where('title', $postTitle)->first();
-        $this->assertNotNull($post);
-
-        $this->assertTrue($post->tags->contains($tag1));
-        $this->assertTrue($post->tags->contains($tag2));
-
     }
 }
